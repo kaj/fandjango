ccb5824be1d9622c50e33d2daa16a7e5a128fbc2
kaj <kaj@kth.se>
2010-01-17 23:25:44 +0000

    Lite enkel webbdesign, med en bastemplate.

--- libris/views.py
+++ libris/views.py
@@ -1,25 +1,45 @@
 from fandjango.libris.models import *
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
-from django.db.models import Count
+from django.db.models import Count, Min
+
+def ctx(**kwargs):
+    '''Utility function for getting a rendering context.'''
+    props = { }
+    props.update(kwargs)
+    if 'headtitle' not in props:
+        props['headtitle'] = u'%s - %s' % (props['pagetitle'], 'Fandjango')
+    return props
+    
+def orderEpisodeQuery(query):
+    '''Take a query for Episodes and make it ordered by first publication.'''
+    return query.annotate(Min('publication__issue__year')).annotate(Min('publication__issue__number')).order_by('publication__issue__number__min').order_by('publication__issue__year__min')
 
 def index(request):
     years = Issue.objects.order_by('year').distinct().values_list('year', flat=True)
     titles = Title.objects.order_by('title').all()
     refs = RefKey.objects.order_by('title').annotate(Count('episode')).all()
-    return render_to_response('index.html', {'years': years,
-                                             'titles': titles,
+    return render_to_response('index.html', {'years': years, 'titles': titles,
                                              'refs': refs})
 def year(request, year):
     issues = Issue.objects.filter(year=year)
-    return render_to_response('year.html', {'year': year, 'issues': issues})
+    years = list(Issue.objects.order_by('year').distinct().values_list('year', flat=True))
+    i = years.index(int(year))
+    n = 2
+    navyears = years[max(i-n,0):i+n+1]
+    return render_to_response('year.html', ctx(year=year, issues=issues,
+                                               navyears=navyears,
+                                               pagetitle='Fantomen %s' %(year)))
 
 def title(request, slug):
     title = get_object_or_404(Title, slug=slug)
-    return render_to_response('title.html', {'title': title})
+    episodes = orderEpisodeQuery(title.episode_set).all()
+    return render_to_response('title.html', ctx(title=title, episodes=episodes,
+                                                pagetitle=unicode(title)))
 
 def refKey(request, slug):
     refkey = get_object_or_404(RefKey, slug=slug)
-    titles = refkey.episode_set.order_by('publication').all()
-    return render_to_response('refkey.html', {'refkey': refkey,
-                                              'titles': titles})
+    episodes = orderEpisodeQuery(refkey.episode_set).all()
+    return render_to_response('refkey.html', ctx(refkey=refkey, 
+                                                 episodes=episodes,
+                                                 pagetitle=str(refkey)))
