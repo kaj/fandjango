e183e46db8fd02156e57c4d0d4a3b75ab1712804
Rasmus Kaj <kaj@kth.se>
2014-09-16 06:41:54 +0200

    Updated aliases.

--- libris/alias.py
+++ libris/alias.py
@@ -4,6 +4,7 @@ def aliases(name, *alias):
 
 ALIASES = dict(
     aliases(u'Alan Gordon', u'Al Gordon', u'Gordon') +
+    aliases(u'Alex Saviuk', u'Saviuk') +
     aliases(u'Alf Woxnerud', u'Wox') +
     aliases(u'Alfredo Castelli', u'Castelli') +
     aliases(u'Anders Eklund', u'A. Eklund') +
@@ -11,7 +12,6 @@ ALIASES = dict(
     aliases(u'Anette Salmelin', u'A. Salmelin') +  
     aliases(u'Ann Schwenke', u'A. Schwenke') +
     aliases(u'Anniqa Tjernlund', u'A. Tjernlund') +  
-    aliases(u'Arthur Conan Doyle', u'A. Conan Doyle') +
     aliases(u'Bengt Sahlberg', u'B. Sahlberg') +
     aliases(u'Bertil Wilhelmsson', u'B. Wilhelmsson', u'Wilhelmsson') +
     aliases(u'Birgit Lundborg', u'Biggan Lundborg', u'B. Lundborg') +
@@ -24,11 +24,13 @@ ALIASES = dict(
     aliases(u'Donne Avenell', u'Don Avenell', u'D. Avenell', u'Avenell') +
     aliases(u'Eirik Ildahl', u'Eiric Ildahl') +
     aliases(u'Eugenio Mattozzi', u'E. Mattozzi') +
+    aliases(u'Falco Pellerin', u'F. Pellerin', u'Terje Nordberg') +
     aliases(u'Ferdinando Tacconi', u'Fernanino Tacconi', u'Tacconi') +
     aliases(u'Fred Fredericks', u'Fredericks') +
     aliases(u'G. Rosinski', u'Rosinski') +  
     aliases(u'Georges Bessis', u'Georges Bess', u'G. Bess') +
     aliases(u'Germano Ferri', u'Ferri') +
+    aliases(u'Grzegorz Rosinski', u'G. Rosinski') +
     aliases(u'Hans Jonsson', u'Hans Jonson', u'Hasse Jonsson', u'H. Jonsson', u'H Jonsson') +
     aliases(u'Hans Lindahl', u'Hasse Lindahl', u'H. Lindahl', u'Lindahl') +
     aliases(u'Heiner Bade', u'Helmer Bade', u'H. Bade', u'H Bade', u'H. Baade', u'Bade') + 
@@ -36,13 +38,12 @@ ALIASES = dict(
     aliases(u'Idi Kharelli', u'Kharelli') +
     aliases(u'Iréne Gasc', u'Irene Gasc') +
     aliases(u'Iván Boix', u'Ivàn Boix', u'Ivan Boix') +
-    aliases(u'J. Van Hamme', u'J Van Hamme', u'Van Hamme') +
     aliases(u'Jaime Vallvé', u'J. Vallvé', u'Vallvé') +
     aliases(u'Janne Lundström', u'Jan Lundström', u'J. Lundström', u'Lundström') +
     aliases(u'Jean Giraud', u'J. Giraud', u'Giraud') +  
+    aliases(u'Jean Van Hamme', u'J. Van Hamme', u'J Van Hamme', u'Van Hamme') +
     aliases(u'Jean-Michel Charlier', u'J-M. Charlier', u'J-M Charlier', u'Charlier') +
     aliases(u'Jean-Yves Mitton', u'J-Y Mitton', u'Mitton') +
-    aliases(u'John Bull', u'J. Bull') +
     aliases(u'Kari Leppänen', u'Kari T. Leppänen', u'Kari T Leppänen', u'Kari Leppänän', u'Kari Läppänen', u'Kari Läppenen', u'K. Leppänen', u'Leppänen') +  
     aliases(u'Karin Bergh', u'K. Bergh') +
     aliases(u'Knut Westad', u'K. Westad', u'Westad') +
@@ -61,7 +62,7 @@ ALIASES = dict(
     aliases(u'Michael Tierres', u'M. Tierres', u'Tierres') +
     aliases(u'Mèziéres', u'Mézières') +	
     aliases(u'Nils Schröder', u'Schröder') +
-    aliases(u'Norman Worker', u'N. Worker', u'Worker') +
+    aliases(u'Norman Worker', u'N. Worker', u'Worker', u'John Bull', u'J. Bull') +
     aliases(u'Ola Westerberg', u'O. Westerberg') +  
     aliases(u'Patrik Norrman', u'Patrik Norman') +
     aliases(u'PeO Carlsten', u'Peo Carlsten') +
@@ -69,6 +70,7 @@ ALIASES = dict(
     aliases(u'Robert Kanigher', u'Bob Kanigher', u'R. Kanigher') +
     aliases(u'Romano Felmang', u'R. Felmang', u'Felmang', u'Roy Mann', u'Mangiarano') +
     aliases(u'Scott Goodall', u'S. Goodall', u'Goodall', u'Scott Godall') +
+    aliases(u'Sir Arthur Conan Doyle', u'Arthur Conan Doyle', u'A. Conan Doyle', u'Sir A. Conan Doyle') +
     aliases(u'Stefan Nagy', u'S. Nagy') +
     aliases(u'Steve Ditko', u'S. Ditko') +
     aliases(u'Sverre Årnes', u'Årnes') +
@@ -79,9 +81,11 @@ ALIASES = dict(
     aliases(u'Tony De Paul', u'Tony DePaul', u'Tony de Paul', u'De Paul', u'DePaul') +  
     aliases(u'Tony De Zuniga', u'Tony de Zuniga') +
     aliases(u'Ulf Granberg', u'U. Granberg', u'Granberg') +
-    aliases(u'Usam', u'Umberto Samarini', u'Umberto Sammarini') +  
+    aliases(u'Usam', u'Umberto Samarini', u'Umberto Sammarini') +
+    aliases(u'Wally Wood', u'Wallace Wood') +
     aliases(u'William Vance', u'W. Vance', u'Vance') +
     aliases(u'Wilson McCoy', u'Wilson Mc Coy', u'McCoy') +
+    aliases(u'Yves Sente', u'Y. Sente') +
     aliases(u'Zane Grey', u'Zane Gray', u'Zane Grej') +
     aliases(u'Özcan Eralp', u'Öscan Eralp', u'Ö. Eralp', u'Eralp')
     )
