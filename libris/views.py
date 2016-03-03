8bf6aad21201a65e199f2dca0d1efe86544c34da
Rasmus Kaj <kaj@kth.se>
2016-03-03 21:17:16 +0100

    Better way to list bulk roles.

--- libris/views.py
+++ libris/views.py
@@ -156,7 +156,9 @@ def refKeys(request):
 def creator(request, slug):
     creator = get_object_or_404(Creator, slug=slug)
     q=CreativePart.objects.filter(creator=creator)
-    episodes = orderEpisodeQuery(Episode.objects.filter(creativepart__in=q)) \
+    main = {'', 'text', 'bild', 'ink', 'orig'}
+    episodes = orderEpisodeQuery(Episode.objects.filter
+                                 (creativepart__in=q.filter(role__in=main))) \
         .select_related('title') \
         .select_related('orig_name') \
         .prefetch_related('creativepart_set__creator') \
@@ -169,8 +171,22 @@ def creator(request, slug):
     else:
         articles = None
     wa = Article.objects.filter(creators=creator)
+    xe = orderEpisodeQuery(Episode.objects.filter
+                           (creativepart__in=q.exclude(role__in=main))).all() \
+        .select_related('title')
+
+    xcred = {t: [ee for ee in xe if ee.title==t] for t in {e.title for e in xe}}
+    ROLE = {
+        'color': u'färgläggare',
+        'redax': u'redaktion',
+        'xlat': u'översättare',
+        'textning': u'textsättare',
+    }
+    xroles = ', '.join(ROLE[r] for r in {p.role for p in q if p.role not in main})
     return render_to_response('creator.html', ctx(creator=creator,
                                                   episodes=episodes,
+                                                  xcred=xcred,
+                                                  xroles=xroles,
                                                   articles=articles,
                                                   writtenarticles=wa,
                                                   pagetitle=unicode(creator)))
