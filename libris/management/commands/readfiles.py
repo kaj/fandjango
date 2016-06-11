b9072371b6a08e69dccf681ee98b0f3e8d09bcc5
Rasmus Kaj <kaj@kth.se>
2016-06-11 14:31:50 +0200

    Update to python 3
    * Use str(..) rather than unicode(..) and implement __str__(self)
      rather than __unicode__(self).
    
    * Integer division is "a // b" rather than "a / b" in python 3.
    
    * Use xml.etree.ElementTree instead of xml.dom.minidom, so I can use
      supported methods rather than to keep porting minixpath.
    
    * print is a function (add parenthesis).

--- libris/management/commands/readfiles.py
+++ libris/management/commands/readfiles.py
@@ -2,8 +2,7 @@ from datetime import datetime
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from libris.models import *
-from xml.dom.minidom import parse
-from minixpath import *
+import xml.etree.ElementTree as ET
 from os import path
 from optparse import make_option
 
@@ -21,10 +20,10 @@ class Command(BaseCommand):
         for year in years:
             file = path.join(dir, '%s.data' % year)
             if path.exists(file):
-                print "Should read from", file
+                print("Should read from %s" % file)
                 read_data_file(file)
             else:
-                print "No such file: %s" % file
+                print("No such file: %s" % file)
         # Clean up stray data
         Article.objects.filter(publication=None).delete()
         Episode.objects.filter(publication=None).delete()
@@ -36,21 +35,21 @@ class Command(BaseCommand):
         kh.delete()
 
 def getBestPlac(elem):
-    bestElem = evaluate(elem, '/best')
-    if bestElem:
-        return int(bestElem[0].getAttribute('plac'))
+    bestElem = elem.find('best')
+    if bestElem != None:
+        return int(bestElem.get('plac'))
     else:
         return 0
 
 def read_data_file(filename):
-    dom = parse(filename)
-    year = getText(evaluate(dom, "/libris/info/year")[0]);
-    for issueElement in evaluate(dom, "/libris/issue"):
-        coverElem = evaluate(issueElement, '/omslag')
-        pages = issueElement.getAttribute('pages') or None
-        price =  issueElement.getAttribute('price') or None
-        cover_best = getBestPlac(coverElem[0]) if coverElem else 0
-        nrattr = issueElement.getAttribute("nr")
+    et = ET.parse(filename)
+    year = et.find('info/year').text;
+    for issueElement in et.findall('issue'):
+        pages = issueElement.get('pages')
+        price =  issueElement.get('price')
+        nrattr = issueElement.get('nr')
+        coverElem = issueElement.find('omslag')
+        cover_best = getBestPlac(coverElem) if coverElem != None else 0
         issue, issue_is_new = Issue.objects.get_or_create(
             year=year,
             number=issueNr(nrattr),
@@ -65,76 +64,73 @@ def read_data_file(filename):
         # Purge before creating new content
         issue.publication_set.all().delete()
         ordno = 0;
-        print "Found issue:", issue
-        for item in issueElement.childNodes:
+        print("Found issue:", issue)
+        for item in issueElement.findall('*'):
             ordno = ordno + 1
-            if item.nodeType != 1:
-                continue
-            if item.tagName == 'omslag':
-                cbe = evaluate(item, '/by')
-                if cbe:
+            if item.tag == 'omslag':
+                cbe = item.find('by')
+                if cbe != None:
                     issue.cover_by = [Creator.objects.get_or_create(name=name)[0]
                                       for name, alias
                                       in [name_alias(who) for who
-                                          in getByWho(*cbe)]]
+                                          in getByWho(cbe)]]
                 
-            elif item.tagName == 'serie':
+            elif item.tag == 'serie':
                 title, title_is_new = Title.objects.get_or_create(
-                    title=getText(evaluate(item, "/title")[0]))
+                    title=text(item.find('title')))
                 if title_is_new:
-                    print "FOUND NEW TITLE:", title
-                episodename = getText(*evaluate(item, "/episode")[:1]) or ''
+                    print("FOUND NEW TITLE:", title)
+                episodename = text(item.find('episode')) or ''
                 part_no, part_name = None, ''
-                partElem = evaluate(item, "/part")
-                if partElem:
-                    no = partElem[0].getAttribute("no");
+                partElem = item.find('part')
+                if partElem != None:
+                    no = partElem.get('no')
                     if no:
                         part_no = no
-                    part_name = getText(partElem[0]);
+                    part_name = text(partElem)
                 episode, is_new_episode = Episode.objects.get_or_create(
                     title=title, episode=episodename)
-                episode.teaser = getText(*evaluate(item, "/teaser")) or \
-                                 episode.teaser
-                episode.note = getText(*evaluate(item, "/note")) or \
-                               episode.note
-                episode.copyright = getText(*evaluate(item, '/copyright')) or \
+                episode.teaser = text(item.find('teaser')) or episode.teaser
+                episode.note = text(item.find('note')) or episode.note
+                episode.copyright = text(item.find('copyright')) or \
                                     episode.copyright
                 
-                origNameElem = evaluate(item, '/episode[@role ="orig"]')
-                if origNameElem and not episode.orig_name:
-                    print "FOUND ORIGNAME", origNameElem
+                origNameElem = item.find('episode[@role="orig"]')
+                if origNameElem != None and not episode.orig_name:
+                    print("FOUND ORIGNAME", text(origNameElem),
+                          origNameElem.attrib)
                     episode.orig_name, eon_isnew = \
                         ForeignName.objects.get_or_create(
-                        title=getText(origNameElem[0]),
-                        language=origNameElem[0].getAttribute("xml:lang"))
-                elif origNameElem:
-                    print "FOUND ORIGNAME", origNameElem, "ignoring it."
+                        title=origNameElem.text,
+                        language=origNameElem.get("{http://www.w3.org/XML/1998/namespace}lang"))
+                elif origNameElem != None:
+                    print("FOUND ORIGNAME", origNameElem, "ignoring it.")
                     
-                stripElem = evaluate(item, '/daystrip')
-                if stripElem:
-                    fromdate = evaluate(stripElem[0], '/from')
-                    todate = evaluate(stripElem[0], '/to')
-                    if fromdate and todate:
+                stripElem = item.find('daystrip')
+                if stripElem != None:
+                    fromdate = stripElem.find('from')
+                    todate = stripElem.find('to')
+                    if fromdate != None and todate != None:
                         episode.daystrip = DaystripRun.objects.create(
-                            fromdate=getText(*fromdate),
-                            todate=getText(*todate),
-                            is_sundays=(stripElem[0].getAttribute('d') == 'sun'))
+                            fromdate=fromdate.text,
+                            todate=todate.text,
+                            is_sundays=(stripElem.get('d') == 'sun'))
                     else:
-                        print "Unknown kind of daystrip: %s" % stripElem
+                        print("Unknown kind of daystrip: %s" % stripElem)
                 
                 episode.ref_keys = set.union(getRefKeys(item),
                                              episode.ref_keys.all())
                 
-                for byElem in evaluate(item, "/by"):
-                    role = byElem.getAttribute('role')
+                for byElem in item.findall('by'):
+                    role = byElem.get('role') or ''
                     for who in getByWho(byElem):
                         CreativePart_create(episode, who, role)
 
-                origPubElem = evaluate(item, "/prevpub/date")
-                if origPubElem:
-                    episode.firstpub = getText(*origPubElem)
+                origPubElem = item.find('prevpub/date')
+                if origPubElem != None:
+                    episode.firstpub = text(origPubElem)
                 
-                prevPubElem = evaluate(item, '/prevpub[magazine!=""]')
+                prevPubElem = item.findall('prevpub[magazine!=""]')
                 if prevPubElem:
                     if not episode.prevpub.count():
                         episode.prevpub = [
@@ -147,10 +143,10 @@ def read_data_file(filename):
                              for ppe in prevPubElem]]
                 episode.save();
                 Publication(issue=issue, episode=episode, ordno=ordno,
-                            label=getText(*evaluate(item, "/label")) or '',
+                            label=text(item.find('label')) or '',
                             part_no=part_no, part_name=part_name,
                             best_plac=getBestPlac(item)).save()
-                prevFaElem = evaluate(item, "/prevpub[fa!='']")
+                prevFaElem = item.findall("prevpub[fa!='']")
                 for e in prevFaElem:
                     fa, fastr = issueNrStr(getText(evaluate(e, "/fa")[0]))
                     y = getText(evaluate(e, "/year")[0])
@@ -158,28 +154,30 @@ def read_data_file(filename):
                                                     numberStr=fastr)[0]
                     Publication.objects.get_or_create(episode=episode, issue=i)
                 
-                #print "Serie", episode
-            elif item.tagName == 'text':
+                #print("Serie", episode)
+            elif item.tag == 'text':
                 article = Article.objects.create(
-                    title=getText(*evaluate(item, '/title')),
-                    subtitle=getText(*evaluate(item, '/subtitle')),
-                    note = getText(*evaluate(item, "/note")))
+                    title=text(item.find('title')),
+                    subtitle=text(item.find('subtitle')) or '',
+                    note = text(item.find('note')) or '')
                 keys = getRefKeys(item)
                 if keys:
                     article.ref_keys = keys
-                creators = evaluate(item, "/by")
+                creators = item.findall('by')
                 if creators:
+                    from itertools import chain
                     article.creators = [Creator.objects.get_or_create(name=name)[0]
                                         for name, alias
                                         in [name_alias(who) for who
-                                            in getByWho(*creators)]]
+                                            in chain.from_iterable(
+                                                getByWho(creator)
+                                                for creator in creators)]]
                 if keys or creators:
                     article.save()
                 Publication(issue=issue, article=article, ordno=ordno).save()
                 
             else:
-                print 'Element', item.tagName
-    dom.unlink()
+                print('Element', item.tag)
 
 def issueNrStr(nrstr):
     t = nrstr.split('/', 1)[0].split(', ', 1)[0]
@@ -190,25 +188,30 @@ def issueNr(nrstr):
     return int(nrstr.split('-',1)[0].split('/',1)[0].split(', ',1)[0])
 
 def getByWho(byElem):
-    whoElem = evaluate(byElem, "/who")
+    if byElem == None:
+        return []
+    whoElem = byElem.findall('who')
     if whoElem:
-        return [getText(who) for who in whoElem]
+        return [text(who) for who in whoElem]
     else:
-        return [getText(byElem)]
+        return [text(byElem)]
 
 def getRefKeys(item):
-    ref = evaluate(item, "/ref")
-    if ref:
-        keys = set.union(set(RefKey.FA(k.getAttribute('no'))
-                             for k in evaluate(ref[0], '/fa')),
-                         set(RefKey.TITLE(getText(s))
-                             for s in evaluate(ref[0], '/serie')),
-                         set(RefKey.WHO(getText(k))
-                             for k in evaluate(ref[0], '/who')),
-                         set(RefKey.KEY(getText(k))
-                             for k in evaluate(ref[0], '/key')))
-        #print "  keys:", keys
+    ref = item.find('ref')
+    if ref != None:
+        keys = set.union(
+            set(RefKey.FA(k.get('no')) for k in ref.findall('fa')),
+            set(RefKey.TITLE(text(s)) for s in ref.findall('serie')),
+            set(RefKey.WHO(text(k)) for k in ref.findall('who')),
+            set(RefKey.KEY(text(k)) for k in ref.findall('key')))
+        #print("  keys:", keys)
         return keys
     else:
         return set()
+
+def text(elem):
+    if elem != None:
+        return ''.join(elem.itertext())
+    else:
+        return None
 
