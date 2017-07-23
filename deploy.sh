f322b8fbe3e434d736908d05c9b0216eec58a9a7
Rasmus Kaj <kaj@kth.se>
2017-07-23 15:48:04 +0200

    Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+SERVER=guran.krats.se
+PROJECT=fandjango
+VERSION=$1
+REPO='git@github.com:kaj/fandjango.git'
+PROJROOT=/usr/web/deploy/$PROJECT
+DESTDIR=$PROJROOT/$VERSION
+
+if [ ! -n "$PROJECT" -o ! -n "$VERSION" ]; then
+    echo "Usage"
+    echo "$0 version"
+    exit
+fi
+echo "Should deploy $PROJECT version $VERSION"
+
+ssh $SERVER <<EOF
+set -e
+if [ -d $DESTDIR ]; then
+  echo "$DESTDIR already exists. Stop."
+  exit 1
+fi
+cd $PROJROOT
+# Checking out "master", but to a directory named $VERSION
+git clone -b master $REPO $VERSION
+cd $VERSION
+pyvenv-3.4 --system-site-packages ve
+. ve/bin/activate
+pip install -r requirements.txt
+cp /usr/web/$PROJECT/settings/local.py settings/
+./manage.py collectstatic --no-input
+ln -s $PROJROOT/covers/* static
+./manage.py fetchcovers
+cd /usr/web
+rm $PROJECT
+ln -sf $DESTDIR $PROJECT
+EOF
